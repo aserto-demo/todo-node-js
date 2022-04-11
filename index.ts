b0d2c37280d956c514744c957b7527943bd46ff9
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2022-04-11 13:47:43 -0700

    No authorization

--- index.ts
+++ index.ts
@@ -1,25 +1,11 @@
 import { Todo } from "./interfaces";
-
 import express = require("express");
 import cors = require("cors");
 import jwt = require("express-jwt");
 import jwksRsa = require("jwks-rsa");
-import aserto = require("express-jwt-aserto");
 import { getUserByUserID } from "./directory";
 import { initDb, getTodos, insertTodo, updateTodo, deleteTodo } from "./store";
 import { UserCache, User } from "./interfaces";
-const { jwtAuthz } = aserto;
-
-const authzOptions = {
-  authorizerServiceUrl: process.env.AUTHORIZER_SERVICE_URL,
-  policyId: process.env.POLICY_ID,
-  policyRoot: process.env.POLICY_ROOT,
-  authorizerApiKey: process.env.AUTHORIZER_API_KEY,
-  tenantId: process.env.TENANT_ID,
-};
-
-//Aserto authorizer middleware function
-const checkAuthz: express.Handler = jwtAuthz(authzOptions);
 
 const checkJwt: jwt.RequestHandler = jwt({
   // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
@@ -45,7 +31,7 @@ const PORT = 3001;
 //Users cache
 const users: UserCache = {};
 
-app.get("/user/:userID", checkJwt, checkAuthz, async (req, res) => {
+app.get("/user/:userID", checkJwt, async (req, res) => {
   const { userID } = req.params;
   const user: User = users[userID] ? users[userID] : await getUserByUserID(userID);
 
@@ -54,7 +40,7 @@ app.get("/user/:userID", checkJwt, checkAuthz, async (req, res) => {
   res.json(user);
 });
 
-app.get("/todos", checkJwt, checkAuthz, async (req, res) => {
+app.get("/todos", checkJwt, async (req, res) => {
   try {
     const todos: Todo[] = await getTodos();
     res.json(todos);
@@ -63,7 +49,7 @@ app.get("/todos", checkJwt, checkAuthz, async (req, res) => {
   }
 });
 
-app.post("/todo", checkJwt, checkAuthz, async (req, res) => {
+app.post("/todo", checkJwt, async (req, res) => {
   const todo: Todo = req.body;
   try {
     await insertTodo(todo);
@@ -73,11 +59,7 @@ app.post("/todo", checkJwt, checkAuthz, async (req, res) => {
   }
 });
 
-app.put(
-  "/todo/:ownerID",
-  checkJwt,
-  checkAuthz,
-  async (req, res) => {
+app.put("/todo/:ownerID", checkJwt, async (req, res) => {
     const todo: Todo = req.body;
     try {
       await updateTodo(todo);
@@ -88,11 +70,7 @@ app.put(
   }
 );
 
-app.delete(
-  "/todo/:ownerID",
-  checkJwt,
-  checkAuthz,
-  async (req, res) => {
+app.delete("/todo/:ownerID", checkJwt, async (req, res) => {
     const todo: Todo = req.body;
     try {
       deleteTodo(todo);
